6a36000b4443fbcbe6a40c0f2211f6cd8e84d5c6
zerosnacks <zerosnacks@protonmail.com>
2024-11-05 11:30:53 +0100

    init

---
+++ src/Counter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC721SeaDrop} from "seadrop/src/ERC721SeaDrop.sol";
+
+contract Counter is ERC721SeaDrop {
+    uint256 public number;
+
+    address[] public a;
+
+    constructor() ERC721SeaDrop("Counter", "COUNT", a) {}
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
